49d2682350d0b3d5cdbdc839b1a17359f84aca27
MoinulIslam7 <moinul.islam@gigalogy.com>
2025-06-15 11:21:09 +0600

    Feat: update experience

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -4,40 +4,40 @@ import * as React from 'react';
 function ProjectCard({ project }) {
   console.log(project);
   return (
-    <div class="card">
-      <div class="card__border"></div>
+    <div className="card">
+      <div className="card__border"></div>
       <div className="w-full h-[14rem]"><img className="w-full h-full rounded-lg" src={project.image.src} alt="" /></div>
-      <div class="card_title__container">
-        <span class="card_title">{project.name}</span>
-        <p class="card_paragraph">
+      <div className="card_title__container">
+        <span className="card_title">{project.name}</span>
+        <p className="card_paragraph">
           {project.tools}
         </p>
       </div>
-      <hr class="line" />
-      <ul class="card__list">
+      <hr className="line" />
+      <ul className="card__list">
         {
           project.shortDescripton.map((tool) => (
-            <li class="card__list_item">
-              <span class="check">
+            <li className="card__list_item">
+              <span className="check">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 16 16"
                   fill="currentColor"
-                  class="check_svg"
+                  className="check_svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
               </span>
-              <span class="list_text">{tool}</span>
+              <span className="list_text">{tool}</span>
             </li>
           ))
         }
       </ul>
-      <button class="button">Click for Blow!</button>
+      <button className="button">Click for Blow!</button>
     </div>
 
   );
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,12 +1,24 @@
 export const experiences = [
   {
     id: 1,
+    title: 'Software Engineer',
+    company: "Gigalogy",
+    duration: "(16 April, 2025 - Present)"
+  },
+  {
+    id: 2,
+    title: 'Developer',
+    company: "Core Devs Ltd.",
+    duration: "(01 December, 2024 - 15 April, 2025)"
+  },
+  {
+    id: 3,
     title: 'Junior Developer',
     company: "Core Devs Ltd.",
     duration: "(18 August, 2023 - Present)"
   },
   {
-    id: 3,
+    id: 4,
     title: "MERN Stack Developer Intern",
     company: "Core Devs Ltd.",
     duration: "(17 Feb, 2023 - 17 August, 2023)"
