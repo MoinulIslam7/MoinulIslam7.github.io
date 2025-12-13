b5c6869192d7491bcaba27c1e823cd8655446295
MoinulIslam7 <moinul.islam@gigalogy.com>
2025-12-13 23:02:48 +0600

    fix

--- next.config.js
+++ next.config.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
+    unoptimized: true,
     domains: ['res.cloudinary.com', 'media.dev.to'],
   },
 };
