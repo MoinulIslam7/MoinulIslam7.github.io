992c8fd7a6db07f7022929177857b071cc045a5f
MoinulIslam7 <moinul.islam@gigalogy.com>
2026-03-24 16:56:11 +0600

    update bio

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Moinul Islam",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "My name is Moinul Islam. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "Hi, I'm Moinul Islam, a driven and self-taught Full Stack Software Engineer currently thriving at Gigalogy. I have 3 years of hands-on development experience, focusing on backend technologies using Python, Node.js, REST APIs, SQL, and NoSQL databases, while also building frontend applications with JavaScript, React, HTML, and CSS. I am passionate about solving complex problems, creating scalable and user-friendly web applications, and continuously learning modern technologies. I am open to software engineering opportunities, collaborations, and projects where I can contribute and grow.",
   email: 'moinul180180@gmail.com',
   phone: '+8801626248996',
   address: 'Mirpur 2, Dhaka, Bangladesh - 1216 ',
